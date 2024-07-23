2a8395526d2a325c3349800ecfae126eb2f2273c
francois6381 <francois.novais@gmail.com>
2024-07-23 12:55:48 +0200

    Container structure and css

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,19 +1,40 @@
 <template>
-  <div>
+  <div class="container">
+    <h2>An awesome title</h2>
     <p>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint minima ipsum animi vitae unde
-      provident itaque. Rem voluptate repellat placeat molestiae veniam officia consequuntur
-      voluptatum nulla, aliquid exercitationem beatae ducimus.
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+      <span>Quibusdam corrupti</span> quisquam minima ducimus magnam magni quod optio amet
+      accusantium ullam, cumque beatae similique officiis. Odit minus nam quasi pariatur inventore?
+      Error, voluptas fuga. Quia quo magnam cum cupiditate quidem nam, facilis temporibus
+      consectetur maxime aspernatur facere est quis. In nemo explicabo vero, voluptatem totam vel
+      aut dolore impedit illo ducimus! Possimus ratione quos cupiditate velit iusto ea earum cumque
+      inventore quaerat nobis. Assumenda mollitia veniam numquam voluptate, sunt officia iure ex
+      corporis nobis itaque voluptatem laborum, <span>animi vel</span>. Eaque, eius? A cumque quod
+      repellat nobis, quo et vel maxime. Sapiente delectus aliquid tempora? Nemo sunt optio commodi
+      rem? Fuga recusandae molestias aliquid sequi accusamus iste quisquam quis, temporibus a vero.
     </p>
     <p>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias perspiciatis veniam obcaecati
-      iusto ducimus magni corporis architecto amet, ratione nulla dignissimos consequatur, assumenda
-      tempore inventore quas itaque consectetur distinctio ab? Voluptate maiores quam modi corporis
-      expedita recusandae voluptas quod totam nostrum repudiandae enim est veritatis adipisci
-      pariatur nisi harum incidunt illum optio excepturi odit fugiat quaerat, ratione dolore
-      laboriosam. Tenetur.
+      iusto ducimus magni corporis architecto amet, ratione nulla
+      <span>dignissimos consequatur</span>, assumenda tempore inventore quas itaque consectetur
+      distinctio ab? Voluptate maiores quam modi corporis expedita recusandae voluptas quod totam
+      nostrum repudiandae enim est veritatis adipisci pariatur nisi harum incidunt illum optio
+      excepturi odit <span>fugiat quaerat</span>, ratione dolore laboriosam. Tenetur.
     </p>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+h2 {
+  font-size: 26px;
+  font-weight: bold;
+  margin: 20px 0;
+  text-decoration: underline var(--main-yellow);
+}
+p {
+  margin-bottom: 20px;
+}
+span {
+  color: var(--main-yellow);
+}
+</style>
