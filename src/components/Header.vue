6c5dd076fbb9a2f0d8df37ac1803aff5115447ff
francois6381 <francois.novais@gmail.com>
2024-07-23 12:13:26 +0200

    header structure and css

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,28 @@
 <template>
-  <div>
-    <img
-      src="https://res.cloudinary.com/lereacteur-apollo/image/upload/v1704448979/RNCP31114/cours-vue/exo-sem-1/my-blog-avatar_xhjc4e.png"
-      alt="Avatar du blog"
-    />
-    <h1>My blog</h1>
-  </div>
+  <header>
+    <div class="header container">
+      <div>
+        <img
+          src="https://res.cloudinary.com/lereacteur-apollo/image/upload/v1704448979/RNCP31114/cours-vue/exo-sem-1/my-blog-avatar_xhjc4e.png"
+          alt="Avatar du blog"
+        />
+      </div>
+      <div><h1>My blog</h1></div>
+    </div>
+  </header>
 </template>
 
-<style scoped></style>
+<style scoped>
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.header img {
+  width: 60px;
+}
+h1 {
+  font-size: 32px;
+  font-weight: bold;
+}
+</style>
